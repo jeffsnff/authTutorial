b2d14bb65820791aeadb8361a82b005995ed7cac
Jeffrey Seneff <jeffsnff@gmail.com>
2021-09-13 09:47:11 -0400

    boiler plate for new project

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config({path: './config.env'})
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on Port : ${PORT}`))
